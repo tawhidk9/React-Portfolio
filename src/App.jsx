3cbaf09bb69e17451dc1d3ea63a977094b37216d
Tawhid <tawhidkamal5@gmail.com>
2023-07-23 17:17:49 -0400

    boilerplate work, added tailwind css, setup vite and basic components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Header from "./components/header"
+import Footer from "./components/footer"
+import Navigation from "./components/Navigation"
+import Project from "./components/project"
+
+
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+      <Header/>
+      <Footer/>
+      <Navigation/>
+      <Project/>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+        <h1 className='text-4xl md:text 7xl mb-1 md:mb-3 font-bold'>Tawhid Kamal</h1>
+        <p>Full Stack Web Developer</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum quidem provident asperiores tenetur temporibus saepe quia rem voluptate iste quasi reprehenderit harum totam, vero veniam? Dolorum dolore reprehenderit vitae saepe.</p>
+    </div>
+  )
+}
+
+export default header
