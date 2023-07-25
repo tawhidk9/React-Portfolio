e217342ddf089d448626618088d7b3f37b4289be
Tawhid <tawhidkamal5@gmail.com>
2023-07-24 22:14:34 -0400

    deployment stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Navigation from "./components/Navigation"
-import Project from "./components/project"
+import Project from "./components/Project"
 import Contact from "./components/Contact"
 
 
