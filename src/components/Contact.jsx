bb5325ea729e59b8f9983af3efda2f8a616ac7ed
Tawhid <tawhidkamal5@gmail.com>
2023-07-24 21:51:12 -0400

    tailwind shenanigans

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,9 +22,10 @@ const Contact = () => {
   return (
     <div id='contact' className='flex flex-col mb-10 mx-auto items-center'>
     <form  className='flex flex-col w-full md:w-7/12 items-center pt-10' onSubmit={handleSubmit}>
-      <div className='px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
+      <div className='px-2 py-1 font-semibold border-2 border-emerald-800 rounded-md'>
         <label htmlFor="name">Name:</label>
         <input
+          className='bg-neutral-500'
           type="text"
           id="name"
           name="name"
@@ -33,9 +34,10 @@ const Contact = () => {
           required
         />
       </div>
-      <div className='px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
-        <label htmlFor="email">Email:</label>
+      <div className='px-2 py-1 font-semibold border-2 border-emerald-800 rounded-md'>
+        <label htmlFor="email" className='bg'>Email:</label>
         <input
+          className='bg-neutral-500'
           type="email"
           id="email"
           name="email"
@@ -44,9 +46,10 @@ const Contact = () => {
           required
         />
       </div>
-      <div className='px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
+      <div className='px-2 py-1 font-semibold border-2 border-emerald-800 rounded-md'>
         <label htmlFor="message">Message:</label>
         <textarea
+          className='bg-neutral-500'
           id="message"
           name="message"
           value={formData.message}
@@ -54,7 +57,7 @@ const Contact = () => {
           required
         />
       </div>
-      <div className='px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
+      <div className='px-2 py-1 font-semibold border-2 border-emerald-800 rounded-md'>
         <button type="submit">Submit</button>
       </div>
     </form>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div id='about' className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
-        <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Tawhid Kamal</h1>
-        <p className='text-base md:text-xl mb-3 font-medium'>Full Stack Web Developer</p>
-        <p className='text-sm max-w-xl mb-6 font-bold'>Make yourself at '/home' (sorry)! NYC born and raised, I'm a personal trainer turned web developer and am looking to hit the ground running in the development field! Little bit about myself, I am a nerd inside and out whether it's aboout eSports, TTRPGs, or LoTR! Feel free to contact me by filling out the contact form. </p>
+    <div id='about' className='flex items-center justify-center flex-col text-center pt-10 pb-6'>
+        <h1 className='text-4xl md:text-6xl mb-1 md:mb-3 font-bold'>Tawhid Kamal</h1>
+        <p className='text-lg md:text-2xl mb-3 font-medium'>Full Stack Web Developer</p>
+        <p className='text-base max-w-xl mb-6 font-medium'>Make yourself at '/home'... (sorry) </p>
+        <p className='text-base max-w-xl mb-6 font-medium'>
+          NYC born and raised, I am a personal trainer turned web developer and am looking to hit the ground running in the development field! Little bit about myself, I am a nerd inside and out whether it's aboout eSports, TTRPGs, or LoTR! Feel free to contact me by filling out the contact form.
+        </p>
+         
     </div>
   )
 }
